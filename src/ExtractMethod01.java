beeeea52a2dce50ab0ab4ade22b4cf02b3f28f17
jinoSon <solod85@gmail.com>
2014-12-10 14:04:14 +0900

    2번

--- src/ExtractMethod01.java
+++ src/ExtractMethod01.java
@@ -5,10 +5,7 @@ public class ExtractMethod01 {
 		Enumeration e = _orders.elements();
 		double outstanding = 0.0;
 		
-		//배너 출력 
-		System.out.println("**********************");
-		System.out.println("******* 고객 외상 *****");	
-		System.out.println("**********************");
+		printBanner();
 		
 		//외상액 계산
 		while(e.hasMoreElements()){
@@ -19,6 +16,13 @@ public class ExtractMethod01 {
 		//세부 내역 출력
 		System.out.println("고객명 : " + _name);
 		System.out.println("외상액 : " + outstanding);
+	}
+	
+	void printBanner(){
+		//배너 출력 
+		System.out.println("**********************");
+		System.out.println("******* 고객 외상 *****");	
+		System.out.println("**********************");
 	}
 
 }
